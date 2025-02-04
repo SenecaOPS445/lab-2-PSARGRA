2825222d967d3f6bdb933abd1d2874375afe43fc
Priyanshu Sargra <psargra@myseneca.ca>
2025-02-04 16:09:23 -0500

    this is lab2f

---
+++ lab2f.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author: Priyanshu Sargra
+# Author ID: 119660223
+# Date Created: 2025/02/03
+
+import sys
+
+
+timer = int(sys.argv[1])
+
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
