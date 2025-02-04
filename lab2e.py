0624a1a837954b2b20b9e9973e0c4983e3d15c86
Priyanshu Sargra <psargra@myseneca.ca>
2025-02-04 16:09:10 -0500

    this is lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Priyanshu Sargra
+# Author ID: 119660223
+# Date Created:2025/02/01
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
